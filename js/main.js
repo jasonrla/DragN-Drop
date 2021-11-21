bd9035bdda8ba799bee3726d52b86b462108bcff
Jason <jasonrla@gmail.com>
2021-11-21 15:28:38 -0500

    test 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const empties = document.querySelectorAll('.empty');
 
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
-
+//hola
 for (const empty of empties) {
   empty.addEventListener('dragover', dragOver);
   empty.addEventListener('dragenter', dragEnter);
